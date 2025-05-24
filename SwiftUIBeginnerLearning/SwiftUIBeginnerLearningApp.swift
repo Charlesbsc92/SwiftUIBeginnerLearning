964589c059cb07353d9cb67ed39617d1ff2b12b3
Charlesbsc92 <charlesbsc92@gmail.com>
2025-05-24 11:37:46 +0530

    Initial Commit

---
+++ SwiftUIBeginnerLearning/SwiftUIBeginnerLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBeginnerLearningApp.swift
+//  SwiftUIBeginnerLearning
+//
+//  Created by Charles on 24/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBeginnerLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
