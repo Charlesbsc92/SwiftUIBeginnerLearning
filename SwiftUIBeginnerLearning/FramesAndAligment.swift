2b74734881572ada6b94d928bed7eac272225cc4
Charlesbsc92 <charlesbsc92@gmail.com>
2025-05-24 11:40:43 +0530

    Frames and Aligment learning

---
+++ SwiftUIBeginnerLearning/FramesAndAligment.swift
@@ -0,0 +1,18 @@
+//
+//  FramesAndAligment.swift
+//  SwiftUIBeginnerLearning
+//
+//  Created by Charles on 24/05/25.
+//
+
+import SwiftUI
+
+struct FramesAndAligment: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FramesAndAligment()
+}
